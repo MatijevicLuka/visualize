73562b97a40d4201eb498f78b841745dd536d02e
Luka Matijevic <luka.matijevic45@gmail.com>
2025-08-19 14:13:33 +0200

    s

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col items-center justify-center h-screen">
     <!-- <h1 class="text-3xl font-bold mb-4">Start here</h1> -->
     <!-- <USeparator color="secondary" class="w-64 mb-6" /> -->
-    <UFileUpload color="secondary" highlight icon="i-lucide-image" label="Upload your CSV file" v-model="value" class="w-96 min-h-48"
+    <UFileUpload highlight label="Upload your CSV file" v-model="value" class="w-96 min-h-48"
       accept="text/csv" />
   </div>
 </template>

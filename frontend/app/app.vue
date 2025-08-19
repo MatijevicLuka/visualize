01376c3deb060d39369e82f893b5bf3be89504dc
Luka Matijevic <luka.matijevic45@gmail.com>
2025-08-19 13:47:25 +0200

    tailwind install, input file added

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -6,3 +6,15 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+
+useHead({
+  title: 'Visio',
+  meta: [
+    { name: 'description', content: 'Visualize your data easily' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+  ]
+});
+
+</script>
--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    <h1>Welcome to the homepage</h1>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <UFileUpload highlight icon="i-lucide-image" label="Upload your CSV file" v-model="value" class="w-96 min-h-48"
+      accept="text/csv" />
   </div>
 </template>
+
+<script setup lang="ts">
+const value = ref(null);
+</script>

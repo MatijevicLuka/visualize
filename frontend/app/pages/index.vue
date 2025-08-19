3b1cbc64ed1d02ada2eb43f12e44c4282e3042db
Luka Matijevic <luka.matijevic45@gmail.com>
2025-08-19 13:54:23 +0200

    primary color

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="flex flex-col items-center justify-center h-screen">
-    <UFileUpload highlight icon="i-lucide-image" label="Upload your CSV file" v-model="value" class="w-96 min-h-48"
+    <!-- <h1 class="text-3xl font-bold mb-4">Start here</h1> -->
+    <!-- <USeparator color="secondary" class="w-64 mb-6" /> -->
+    <UFileUpload color="secondary" highlight icon="i-lucide-image" label="Upload your CSV file" v-model="value" class="w-96 min-h-48"
       accept="text/csv" />
   </div>
 </template>
 
 <script setup lang="ts">
+
 const value = ref(null);
 </script>
